a09c4e968950de2ce727e9db28b21af445167c91
(eng) Scott Silver <ssilver@dhcp-172-31-2-49.kir.corp.google.com>
2012-03-07 14:20:09 -0800

    Add Apache header.

--- src/com/silverware/BinaryReadHelper.java
+++ src/com/silverware/BinaryReadHelper.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2012 Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package com.silverware;
 
 import java.io.DataInputStream;
@@ -7,131 +21,136 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 
 class BinaryReadHelper {
-  DataInputStream stream;
-  
-  /*
-   * Number of bytes read since last call to startCountingBytes or since construction. 
-   */
-  private int counter;
-  
-  /*
-   * Number of bytes read from this instance.
-   */
-  private int offset;
-  
-  public BinaryReadHelper(InputStream input) {
-    stream = new DataInputStream(input);
-  }
-  
-  void matchString(String matchValue, String fieldName) throws IOException {
-    byte[] matchValueBytes = matchValue.getBytes();
-    byte[] fieldValue = new byte[matchValueBytes.length];
+	DataInputStream stream;
 
-    stream.read(fieldValue, 0, fieldValue.length);
+	/*
+	 * Number of bytes read since last call to startCountingBytes or since
+	 * construction.
+	 */
+	private int counter;
 
-    assert (Arrays.equals(matchValueBytes, fieldValue));
+	/*
+	 * Number of bytes read from this instance.
+	 */
+	private int offset;
 
-    counter += fieldValue.length;
-    offset += fieldValue.length;
-  }
+	public BinaryReadHelper(InputStream input) {
+		stream = new DataInputStream(input);
+	}
 
-  void matchByte(byte match, String fieldName) throws IOException {
-    byte fieldByte = stream.readByte();
-    assert (match == fieldByte);
+	void matchString(String matchValue, String fieldName) throws IOException {
+		byte[] matchValueBytes = matchValue.getBytes();
+		byte[] fieldValue = new byte[matchValueBytes.length];
 
-    counter += 1;
-    offset += 1;
-  }
+		stream.read(fieldValue, 0, fieldValue.length);
 
-  // length includes \0
-  String getNullTerminatedString(String fieldName, int length) throws IOException {
-    byte[] field = new byte[length];
+		assert (Arrays.equals(matchValueBytes, fieldValue));
 
-    int bytesRead = stream.read(field, 0, length);
-    assert (bytesRead == length);
+		counter += fieldValue.length;
+		offset += fieldValue.length;
+	}
 
-    counter += length;
-    offset += length;
+	void matchByte(byte match, String fieldName) throws IOException {
+		byte fieldByte = stream.readByte();
+		assert (match == fieldByte);
 
-    // FIXME(ssilver): Wrong charset
-    String string = new String(field, 0, length - 1, Charset.defaultCharset());
-    debugOutput(fieldName, string, length);
-    
-    return string;
-  }
+		counter += 1;
+		offset += 1;
+	}
 
-  private void debugOutput(String fieldName, String fieldValue, int length) {
-    //System.out.format("field: %s at %x: %s\n", fieldName, offset - length, fieldValue);
-  }
+	// length includes \0
+	String getNullTerminatedString(String fieldName, int length)
+			throws IOException {
+		byte[] field = new byte[length];
 
-  int getInt16(boolean bigEndian, String fieldName) throws IOException {
-    int field = stream.readUnsignedShort();
-    int returnField = field;
+		int bytesRead = stream.read(field, 0, length);
+		assert (bytesRead == length);
 
-    if (!bigEndian) {
-      returnField = ((field & 0xff) << 8) | (field >> 8);
-    }
+		counter += length;
+		offset += length;
 
-    counter += 2;
-    offset += 2;
-    debugOutput(fieldName, Integer.toString(returnField), 2);
-    return returnField;
-  }
+		// FIXME(ssilver): Wrong charset
+		String string = new String(field, 0, length - 1,
+				Charset.defaultCharset());
+		debugOutput(fieldName, string, length);
 
-  public int getInt32(boolean bigEndian, String fieldName) throws IOException {
-    int field = stream.readInt();
-    int returnField = field;
+		return string;
+	}
 
-    if (!bigEndian) {
-      returnField = (field >>> 24) | (field << 24) | ((field << 8) & 0xff0000)
-          | ((field >> 8) & 0xff00);
-    }
+	private void debugOutput(String fieldName, String fieldValue, int length) {
+		// System.out.format("field: %s at %x: %s\n", fieldName, offset -
+		// length, fieldValue);
+	}
 
-    counter += 4;
-    offset += 4;
-    debugOutput(fieldName, Integer.toString(returnField), 4);
+	int getInt16(boolean bigEndian, String fieldName) throws IOException {
+		int field = stream.readUnsignedShort();
+		int returnField = field;
 
-    return returnField;
-  }
+		if (!bigEndian) {
+			returnField = ((field & 0xff) << 8) | (field >> 8);
+		}
 
-  public byte getByte(String fieldName) throws IOException {
-    counter += 1;
-    offset += 1;
-    
-    byte returnValue = stream.readByte();
-    
-    debugOutput(fieldName, Byte.toString(returnValue), 1);
-    return returnValue;
-  }
+		counter += 2;
+		offset += 2;
+		debugOutput(fieldName, Integer.toString(returnField), 2);
+		return returnField;
+	}
 
-  public void startCountingBytes() {
-    assert (counter == 0);
-    counter = 0;
-  }
+	public int getInt32(boolean bigEndian, String fieldName) throws IOException {
+		int field = stream.readInt();
+		int returnField = field;
 
-  public int stopCountingBytes() {
-    counter = 0;
-    return counter;
-  }
+		if (!bigEndian) {
+			returnField = (field >>> 24) | (field << 24)
+					| ((field << 8) & 0xff0000) | ((field >> 8) & 0xff00);
+		}
 
-  public int measureBytesRead() {
-    return counter;
-  }
+		counter += 4;
+		offset += 4;
+		debugOutput(fieldName, Integer.toString(returnField), 4);
 
-  public byte[] getBytes(int fieldLength, String fieldName) throws IOException {
-    byte[] fieldBytes = new byte[fieldLength];
+		return returnField;
+	}
 
-    int bytesRead = stream.read(fieldBytes, 0, fieldLength);
-    assert (bytesRead == fieldLength);
+	public byte getByte(String fieldName) throws IOException {
+		counter += 1;
+		offset += 1;
 
-    counter += fieldLength;
-    offset += fieldLength;
-    debugOutput(fieldName, Utility.hexToString(fieldBytes, 0), fieldLength);
+		byte returnValue = stream.readByte();
 
-    return fieldBytes;
-  }
+		debugOutput(fieldName, Byte.toString(returnValue), 1);
+		return returnValue;
+	}
 
-  public long getBytesRead() {
-    return offset;
-  }
+	public void startCountingBytes() {
+		assert (counter == 0);
+		counter = 0;
+	}
+
+	public int stopCountingBytes() {
+		counter = 0;
+		return counter;
+	}
+
+	public int measureBytesRead() {
+		return counter;
+	}
+
+	public byte[] getBytes(int fieldLength, String fieldName)
+			throws IOException {
+		byte[] fieldBytes = new byte[fieldLength];
+
+		int bytesRead = stream.read(fieldBytes, 0, fieldLength);
+		assert (bytesRead == fieldLength);
+
+		counter += fieldLength;
+		offset += fieldLength;
+		debugOutput(fieldName, Utility.hexToString(fieldBytes, 0), fieldLength);
+
+		return fieldBytes;
+	}
+
+	public long getBytesRead() {
+		return offset;
+	}
 }
--- src/com/silverware/DatabaseRecord.java
+++ src/com/silverware/DatabaseRecord.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2012 Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.silverware;
 
 import java.io.PrintWriter;
@@ -10,41 +24,41 @@ import java.util.Iterator;
  */
 public class DatabaseRecord {
 
-  private int handle;
-  private int uniqueId;
-  HashMap<Byte, DatabaseRecordField> fields = new HashMap<Byte, DatabaseRecordField>();
+	private int handle;
+	private int uniqueId;
+	HashMap<Byte, DatabaseRecordField> fields = new HashMap<Byte, DatabaseRecordField>();
 
-  public DatabaseRecord(int databaseRecordHandle, int databaseRecordUniqueId) {
-    handle = databaseRecordHandle;
-    uniqueId = databaseRecordUniqueId;
-  }
+	public DatabaseRecord(int databaseRecordHandle, int databaseRecordUniqueId) {
+		handle = databaseRecordHandle;
+		uniqueId = databaseRecordUniqueId;
+	}
 
-  public DatabaseRecordField addField(int fieldLength, byte fieldType,
-      byte[] fieldData) {
-    DatabaseRecordField field = new DatabaseRecordField(fieldLength, fieldType,
-        fieldData);
+	public DatabaseRecordField addField(int fieldLength, byte fieldType,
+	    byte[] fieldData) {
+		DatabaseRecordField field = new DatabaseRecordField(fieldLength, fieldType,
+		    fieldData);
 
-    fields.put(fieldType, field);
+		fields.put(fieldType, field);
 
-    return field;
-  }
+		return field;
+	}
 
-  public DatabaseRecordField getField(byte fieldType) {
-    return fields.get(fieldType);
-  }
+	public DatabaseRecordField getField(byte fieldType) {
+		return fields.get(fieldType);
+	}
 
-  @Override
-  public String toString() {
-    return "DatabaseRecord [handle=" + handle + ", uniqueId=" + uniqueId
-        + "]\n" + fields;
-  }
+	@Override
+	public String toString() {
+		return "DatabaseRecord [handle=" + handle + ", uniqueId=" + uniqueId
+		    + "]\n" + fields;
+	}
 
-  public void dump(PrintWriter printWriter) {
-    printWriter.printf("  Record h=%d id=%d\n", handle, uniqueId);
-    for (Iterator<DatabaseRecordField> fieldIterator = fields.values()
-        .iterator(); fieldIterator.hasNext();) {
-      DatabaseRecordField field = fieldIterator.next();
-      field.dump(printWriter);
-    }
-  }
+	public void dump(PrintWriter printWriter) {
+		printWriter.printf("  Record h=%d id=%d\n", handle, uniqueId);
+		for (Iterator<DatabaseRecordField> fieldIterator = fields.values()
+		    .iterator(); fieldIterator.hasNext();) {
+			DatabaseRecordField field = fieldIterator.next();
+			field.dump(printWriter);
+		}
+	}
 }
